47041a88cfa3b37eecbd07ecbc1385331af8cfcc
Phat Diep <phdiep@andrew.cmu.edu>
2024-06-13 09:04:34 +0300

    add nav bar and h2 tag

--- asset/JS/scripts.js
+++ asset/JS/scripts.js
@@ -1,3 +1,27 @@
+// JavaScript for dropdown functionality
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownItems = document.querySelectorAll('.header__navbar-dropdown');
+
+    dropdownItems.forEach(function (item) {
+        item.addEventListener('click', function (event) {
+            const dropdownContent = this.querySelector('.header__navbar-dropdown-content');
+            dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+            event.stopPropagation();
+        });
+    });
+
+    // Close dropdown when clicking outside of it
+    window.addEventListener('click', function (event) {
+        dropdownItems.forEach(function (item) {
+            const dropdownContent = item.querySelector('.header__navbar-dropdown-content');
+            if (dropdownContent.style.display === 'block' && !item.contains(event.target)) {
+                dropdownContent.style.display = 'none';
+            }
+        });
+    });
+});
+
+
 var canvasElement = document.getElementById("cookieChart");
 
 var config =  {
