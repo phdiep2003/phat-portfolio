89d299185db494c22ba4911b1fad214da6698113
Phat Diep <phdiep@andrew.cmu.edu>
2024-06-19 08:10:05 +0300

    change charts js

--- asset/JS/scripts.js
+++ asset/JS/scripts.js
@@ -1,47 +1,7 @@
-// Cookie chart
-var canvasElement = document.getElementById("cookieChart");
-
-var config =  {
-    type:"bar",
-    data: {
-        labels:["EV/Sales", "EV/EBITDA","EV/EBIT","P/E"],
-        datasets: [{
-            label:"in USD", 
-            data:[40,50,60,70],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                // 'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(54, 162, 235, 1)',
-                // 'rgba(153, 102, 255, 1)',
-                // 'rgba(201, 203, 207, 1)'
-              ],
-            borderColor: [
-                'rgb(255, 99, 132)',
-                // 'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)'
-                // 'rgb(153, 102, 255)',
-                // 'rgb(201, 203, 207)'
-              ],
-            borderWidth: 1
-            }],
-    },
-    options: {
-        indexAxis: 'y',
-    }
-};
-
-var cookieChart = new Chart (canvasElement,config);
-
-
-// Dark mode
 document.addEventListener('DOMContentLoaded', (event) => {
+    // Dark Mode
     const htmlElement = document.documentElement;
     const switchElement = document.getElementById('darkModeSwitch');
-
     // if local storage has key bsTheme => change theme to value in local storage
     // if not write the current system theme
     // Set the default theme to dark if no setting is found in local storage
@@ -55,27 +15,71 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     switchElement.addEventListener('change', function () {
         if (this.checked) {
-            htmlElement.setAttribute('data-bs-theme', 'dark');
-            localStorage.setItem('bsTheme', 'dark');
+            setTheme('dark');
         } else {
-            htmlElement.setAttribute('data-bs-theme', 'light');
-            localStorage.setItem('bsTheme', 'light');
+            setTheme('light');
         }
     });
 
-    // function updateTheme() {
-    //     document
-    //       .querySelector("html")
-    //       .setAttribute(
-    //         "data-bs-theme",
-    //         window.matchMedia("(prefers-color-scheme: dark)").matches
-    //           ? "dark"
-    //           : "light"
-    //       );
-    //   }
-    //   window
-    //     .matchMedia("(prefers-color-scheme: dark)")
-    //     .addEventListener("change", updateTheme);
-    //   updateTheme();
-  
+    function setTheme(theme) {
+        htmlElement.setAttribute('data-bs-theme', theme);
+        localStorage.setItem('bsTheme', theme);
+        updateChartTheme(theme);
+    }
+
+    function updateChartTheme(theme) {
+        let tickColor = theme === 'dark' ? 'white' : 'black';
+        cookieChart.options.scales.x.ticks.color = tickColor;
+        cookieChart.options.scales.y.ticks.color = tickColor;
+        cookieChart.update();
+    }
+
+    // Cookie chart
+    var canvasElement = document.getElementById("cookieChart");
+
+    var config =  {
+        type: "bar",
+        data: {
+            labels: ["EV/Sales", "EV/EBITDA","EV/EBIT","P/E","Intrinsic Valuation"],
+            datasets: [{
+                label: "USD Price/Share", 
+                data: [[18,160],[13.88,98.03],[11.01,71.85],[14.76,68.02],[55.91,67.59]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 205, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                ],
+                borderWidth: 1
+            }],
+        },
+        options: {
+            plugins: { 
+                legend: {
+                  labels: {color: currentTheme === 'dark' ? 'white' : 'black'}
+                }
+            },
+            scales: {
+                x: {
+                    stacked: true,
+                    ticks: { color: currentTheme === 'dark' ? 'white' : 'black' }
+                },
+                y: {
+                    stacked: true,
+                    ticks: { color: currentTheme === 'dark' ? 'white' : 'black' }
+                }
+            },
+            indexAxis: 'y',
+        }
+    };
+
+    var cookieChart = new Chart(canvasElement, config);
 });
